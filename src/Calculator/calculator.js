15f872877622c87aa8d09b96394827cc16692b15
Tola-lemma <tolalemma@gmail.com>
2023-03-22 12:21:20 +0300

    all button for calculator is set

--- src/Calculator/calculator.js
+++ src/Calculator/calculator.js
@@ -3,10 +3,35 @@ import './calculator.css';
 
 export const  Calculator=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Calculator App</h1>
-      </header>
+    <div className='calculator-background'>
+    <div className="calculator-grid">
+      <div className='output'>
+         <div className='prev-operands'>7</div>
+         <div className='second-operands'>3+4</div>
+      </div>
+      <button>AC</button>
+      <button>DEL</button>
+      <button>+</button>
+
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>/</button>
+
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+
+      <button>0</button>
+      <button>.</button>
+      <button>=</button>
+    </div>
     </div>
   );
 }
