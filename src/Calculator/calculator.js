625266fbb18f8a4babd9d774dd14911e442caa84
Tola-lemma <tolalemma@gmail.com>
2023-03-22 12:04:28 +0300

    initial commit

---
+++ src/Calculator/calculator.js
@@ -0,0 +1,12 @@
+
+import './calculator.css';
+
+export const  Calculator=()=> {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Calculator App</h1>
+      </header>
+    </div>
+  );
+}
